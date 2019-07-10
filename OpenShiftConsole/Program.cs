7847430ae71733de10ad223374a6bcb498f99416
Yamil Ortega <elcorreodeyamil@hotmail.com>
2019-07-10 10:52:15 -0500

    PRimer commit

---
+++ OpenShiftConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OpenShiftConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                            Console.WriteLine("Hello World!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
